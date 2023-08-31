922d236ddce007aa884ca69f458ff0b52a69deed
Jiao Mingye <35512473+mxdzs0612@users.noreply.github.com>
2023-08-31 08:54:10 +0800

    Add Licenses for cpp files (#8)

--- cpp/src/serialization/TrinoSerializer.cpp
+++ cpp/src/serialization/TrinoSerializer.cpp
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "TrinoSerializer.h"
 #include <glog/logging.h>
 #include "velox/common/base/Crc.h"
@@ -1160,9 +1173,9 @@ void readTimestampWithTimeZone(ByteStream* source,
     }
   }
 
-  *result =
-      std::make_shared<RowVector>(pool, TIMESTAMP_WITH_TIME_ZONE(), timestamps->nulls(),
-                                  size, std::vector<VectorPtr>{timestamps, std::move(timezones)});
+  *result = std::make_shared<RowVector>(
+      pool, TIMESTAMP_WITH_TIME_ZONE(), timestamps->nulls(), size,
+      std::vector<VectorPtr>{timestamps, std::move(timezones)});
 }
 
 void readRowVector(ByteStream* source, std::shared_ptr<const Type> type,
