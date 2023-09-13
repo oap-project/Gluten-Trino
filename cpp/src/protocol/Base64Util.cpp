cff84cbe7352ae5ab93a6ea8c864cf289e64aff2
Jiao Mingye <35512473+mxdzs0612@users.noreply.github.com>
2023-09-13 10:04:34 +0800

    Fix occasionally error when reading blocks with null value (#32)

--- cpp/src/protocol/Base64Util.cpp
+++ cpp/src/protocol/Base64Util.cpp
@@ -178,7 +178,7 @@ velox::VectorPtr readVariableWidthBlock(const velox::TypePtr& type, ByteStream& 
   if (nulls) {
     // TBD: Performance optimization
     for (auto i = 0; i < positionCount; ++i) {
-      if (velox::bits::isBitNull(nulls->as<uint64_t>(), i)) {
+      if (!velox::bits::isBitNull(nulls->as<uint64_t>(), i)) {
         rawBuffer[i] = velox::StringView(rawString + offset, rawLens[i]);
         offset += rawLens[i];
       } else {
@@ -345,6 +345,21 @@ velox::VectorPtr readBlockInt(const velox::TypePtr& type, ByteStream& stream,
         pool, velox::TIMESTAMP_WITH_TIME_ZONE(), std::move(nulls), positionCount,
         std::vector<velox::VectorPtr>{std::move(timestamps), std::move(timezones)});
   };
+
+  if (type->kind() == velox::TypeKind::ROW) {
+    auto numFields = stream.read<int32_t>();
+    std::vector<velox::VectorPtr> children;
+    children.reserve(numFields);
+    for (auto i = 0; i < numFields; i++) {
+      children.push_back(readBlockInt(type->asRow().childAt(i), stream, pool));
+    }
+    auto positionCount = stream.read<int32_t>();
+    for (int position = 0; position < positionCount + 1; position++) {
+      stream.read<int32_t>();
+    }
+    velox::BufferPtr nulls = readNulls(positionCount, stream, pool);
+    return std::make_shared<velox::RowVector>(pool, type, nulls, positionCount, children);
+  }
 
   return VELOX_DYNAMIC_SCALAR_TYPE_DISPATCH(readScalarBlock, type->kind(), encoding, type,
                                             stream, pool);
