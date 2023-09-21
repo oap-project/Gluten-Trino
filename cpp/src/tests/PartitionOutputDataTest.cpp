80c37b01579bd4d9463ca25f94f19f03153b064f
枢木 <wuxueyang.wxy@alibaba-inc.com>
2023-09-21 16:53:39 +0800

    fix

--- cpp/src/tests/PartitionOutputDataTest.cpp
+++ cpp/src/tests/PartitionOutputDataTest.cpp
@@ -1,25 +1,24 @@
 /*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <thread>
 
-#include "gtest/gtest.h"
-#include "folly/io/IOBuf.h"
 #include "PartitionOutputData.h"
+#include "folly/io/IOBuf.h"
+#include "gtest/gtest.h"
 
-class PartitionOutputDataTest : public ::testing::Test {
-};
+class PartitionOutputDataTest : public ::testing::Test {};
 
 TEST(PartitionOutputDataTest, basic) {
   io::trino::bridge::PartitionOutputData output;
@@ -38,7 +37,7 @@ TEST(PartitionOutputDataTest, basic) {
 
   auto result = output.pop(10);
   ASSERT_EQ(result.size(), 10);
-  for (auto & buf: result) {
+  for (auto& buf : result) {
     ASSERT_EQ(buf->computeChainDataLength(), 0);
   }
   ASSERT_EQ(output.getOutputDataNum(), 0);
